9cc34b8758f895b54c7a9af76e4b760a4b5ee844
UrielCalazada0525 <89798086+UrielCalazada0525@users.noreply.github.com>
2021-09-06 21:26:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ fuction getIMC(){
   var peso=parceFloat(document.getElementById(´Peso´).value);
   var altura=parseFloat(document.getElementById(´Altura´).value);
   var imc= peso/(altura+altura) 
-  document.getElementbyId("IMC").innerHTML=IMC;
+  document.getElementbyId("imc").innerHTML=imc;
 }
  
